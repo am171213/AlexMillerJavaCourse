ef232579165a19ca38f73505bcc7371c8e9de8d9
Alex Miller <am171213@scots.edinboro.edu>
2021-08-09 15:02:08 -0400

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+package ExamplePackage;
+
+public class Hello 
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Alexander Miller");
+        System.out.println("Phone: (772)-444-5556");
+        System.out.println("Email: alexander.kaspermiller@gmail.com");
+    }
+}
